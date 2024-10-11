4948f286a37e53e40129a78ee6014906c871fd7f
Dell NB <nithi.handsome@gmail.com>
2024-10-11 21:43:18 +0700

    Add Employee Matrix Report

--- Src/RocketShop.Database/Model/NonDatabaseModel/EmployeeMatrixReport.cs
+++ Src/RocketShop.Database/Model/NonDatabaseModel/EmployeeMatrixReport.cs
@@ -8,8 +8,47 @@ using System.Threading.Tasks;
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 namespace RocketShop.Database.Model.NonDatabaseModel
 {
-    public class EmployeeMatrixReport :EmployeeDataReport
+    public class EmployeeMatrixReport 
     {
+        [NHAutoTableSkipColumn]
+        public string UserId { get; set; }
+
+        [NHAutoTableColumnDisplay("Employee Id")]
+        public string EmployeeCode { get; set; }
+
+        public string? Prefix { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+
+        [NHAutoTableTrueDisplay("<i class='check icon' />")]
+        [NHAutoTableFalseDisplay("<i class='x icon' />")]
+        public bool Active { get; set; }
+
+        [NHAutoTableColumnDisplay("Create Date")]
+        [NHAutoTableDateTimeFormatDisplay("yyyy-MM-dd HH:mm:ss")]
+        public DateTime CreateDate { get; set; }
+
+        [NHAutoTableColumnDisplay("Create By")]
+        [NHAutoTableNullDisplay("SYSTEM")]
+        public string? CreateByName { get; set; }
+        public string Email { get; set; }
+
+        [NHAutoTableColumnDisplay("Has Information")]
+        [NHAutoTableTrueDisplay("<i class='check icon' />", "Yes")]
+        [NHAutoTableFalseDisplay("<i class='x icon' />", "No")]
+        public bool HasInformation { get; set; }
+
+        [NHAutoTableColumnDisplay("Has Financial Data")]
+        [NHAutoTableTrueDisplay("<i class='check icon' />", "Yes")]
+        [NHAutoTableFalseDisplay("<i class='x icon' />", "No")]
+        public bool HasFinancial { get; set; }
+        public string? Position { get; set; }
+        public string? Department { get; set; }
+
+        [NHAutoTableColumnDisplay("Manager Name")]
+        [NHAutoTableNullDisplay("No Manager", "No Manager")]
+        public string? ManagerName { get; set; }
+
         [NHAutoTableNewLineSeparator(",")]
         public string Roles { get; set; }
         [NHAutoTableNewLineSeparator(",")]
