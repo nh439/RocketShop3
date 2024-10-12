b7f799501ffbe7cf3a32ccf3e1dcdb5d6c904f15
Dell NB <nithi.handsome@gmail.com>
2024-10-13 01:14:19 +0700

    Implement NHTable Functions

---
+++ Src/RocketShop.SharedBlazor/Domain/NHAutoTableRowCellClickedArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketShop.SharedBlazor.Domain
+{
+    public sealed class NHAutoTableRowCellClickedArgs(string value,string variableName,long row,long column,long position)
+    {
+        public readonly string Value  = value;
+        public readonly string VariableName = variableName;
+        public long Row =row;
+        public long Column =column;
+        public long Position =position;
+        public override string ToString() => $"{VariableName}->{Value},{Position}({Row},{Column})";
+    }
+}
---
+++ Src/RocketShop.SharedBlazor/Domain/NHTableColumnClickedArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketShop.SharedBlazor.Domain
+{
+    public sealed class NHTableColumnClickedArgs (string value,int index)
+    {
+        public readonly string Value = value;
+        public readonly int Index = index;
+    }
+}
