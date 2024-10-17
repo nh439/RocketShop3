7f35fca6323273abc4d505cccadb81273ba1fd5a
Dell NB <nithi.handsome@gmail.com>
2024-10-17 21:29:03 +0700

    Add CallAudit Function

---
+++ Src/RocketShop.AuditService/Domain/ActivityLogAdvanceSearch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketShop.AuditService.Domain
+{
+    public class ActivityLogAdvanceSearch
+    {
+        public DateTime? DateFrom {  get; set; }
+        public DateTime? DateTo { get; set; }
+        public string? Actor {  get; set; }
+        public string? Division { get; set; }
+        public string? ServiceName { get; set; }
+        public string? Email { get; set; }
+    }
+}
