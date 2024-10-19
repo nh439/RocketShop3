7ac47ee9390a9d8642c4072a727dcd670ab3a1b3
Dell NB <nithi.handsome@gmail.com>
2024-10-20 01:00:02 +0700

    Add Framework Test

---
+++ Test/RocketShop.IntegrationTest/Framework/Extension/EnumerableBooleanTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using RocketShop.Framework.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketShop.IntegrationTest.Framework.Extension
+{
+    [TestClass]
+    public class EnumerableBooleanTest
+    {
+        [TestMethod]
+        public void AllTrueTest_ReturnTrue()
+        {
+            var initial = new[] { true, true, true, true, true, true, true, true };
+            var result = initial.AllTrue();
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        [DataRow(new[] { true,true,false,true})]
+        [DataRow(new[] { false,false,false,false})]
+        public void AllTrueTest_ReturnFalse(bool[] initial)
+        {
+            var result = initial.AllTrue();
+            result.Should().BeFalse();
+        }
+    }
+}
