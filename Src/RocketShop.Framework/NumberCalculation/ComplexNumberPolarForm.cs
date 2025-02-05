2bb17f7769d388eb113105a41acbe3336ead8bfb
Dell NB <nithi.handsome@gmail.com>
2025-02-06 00:52:23 +0700

    Add Hexadecimal Class

---
+++ Src/RocketShop.Framework/NumberCalculation/ComplexNumberPolarForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketShop.Framework.NumberCalculation
+{
+    public struct ComplexNumberPolarForm
+    {
+        public double Magnitude { get; set; }
+        public double Degrees { get; set; }
+        public ComplexNumberPolarForm(double magnitude, double degrees)
+        {
+            Magnitude = magnitude;
+            if(degrees > 360)
+            {
+                Degrees = degrees % 360;
+            }
+            else
+            {
+                Degrees = degrees;
+            }
+        }
+        public ComplexNumberPolarForm()
+        {
+            Magnitude = 0;
+            Degrees = 0;
+        }
+        public ComplexNumberPolarForm(Complex complex)
+        {
+            Magnitude = complex.Magnitude;
+            Degrees = complex.Phase * (180 / Math.PI);
+        }
+
+        public ComplexNumberPolarForm(string value)
+        {
+            var parts = value.Split('∠');
+            Magnitude = double.Parse(parts[0]);
+            Degrees = double.Parse(parts[1].Replace("°", ""));
+        }
+
+        public static implicit operator Complex(ComplexNumberPolarForm polar) =>
+            new Complex(polar.Magnitude * Math.Cos(polar.Degrees), polar.Magnitude * Math.Sin(polar.Degrees));
+
+        public static implicit operator ComplexNumberPolarForm(Complex complex) =>
+            new ComplexNumberPolarForm(complex);
+
+        public override string ToString() => $"{Magnitude} ∠ {Degrees}°";
+
+
+
+    }
+}
---
+++ Src/RocketShop.Framework/NumberCalculation/Hexadecimal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketShop.Framework.NumberCalculation
+{
+    public struct Hexadecimal
+    {
+        public string Value { get; init; }
+        public Hexadecimal(string value)
+        {
+            HashSet<char> allowedChars = new HashSet<char>("1234567890ABCDEF");
+            bool isValid = true;
+            foreach (char c in value)
+            {
+                if (!allowedChars.Contains(c))
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid)
+            {
+                Value = value.ToUpper();
+            }
+            else
+            {
+                throw new ArgumentException("Invalid hexadecimal value");
+            }
+        }
+        public Hexadecimal(byte[] data)
+        {
+            string hex = BitConverter.ToString(data).Replace("-", "");
+            Value = hex;
+        }
+        public byte[] ToByteArray()
+        {
+            byte[] data = new byte[Value.Length / 2];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = Convert.ToByte(Value.Substring(i * 2, 2), 16);
+            }
+            return data;
+        }
+        public string ToBase64String() => Convert.ToBase64String(ToByteArray());
+
+
+        public static implicit operator Hexadecimal(byte value) => new Hexadecimal(value.ToString("X"));
+        public static implicit operator Hexadecimal(short value) => new Hexadecimal(value.ToString("X"));
+        public static implicit operator Hexadecimal(int value) => new Hexadecimal(value.ToString("X"));
+        public static implicit operator Hexadecimal(long value) => new Hexadecimal(value.ToString("X"));
+        public static implicit operator Hexadecimal(Int128 value) => new Hexadecimal(value.ToString("X"));
+
+        public override string ToString() => Value;
+    }
+}
