e3a8cf8dc062b0099299379027d7dd89f68f63ca
Dell NB <nithi.handsome@gmail.com>
2025-02-09 22:00:29 +0700

    Update COPY Function

--- Src/RocketShop.Database/Extension/PostgreSqlExtension.cs
+++ Src/RocketShop.Database/Extension/PostgreSqlExtension.cs
@@ -1,7 +1,10 @@
-﻿using LanguageExt;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using LanguageExt;
+using RocketShop.Framework.Extension;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,69 +13,138 @@ namespace RocketShop.Database.Extension
 {
     public static class PostgreSqlExtension
     {
+        /// <summary>
+        /// Copies binary data into a database table asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The type of the data to copy.</typeparam>
+        /// <param name="connection">The database connection to use.</param>
+        /// <param name="tableName">The name of the table to insert data into.</param>
+        /// <param name="columnToInsert">The column in which the data will be inserted.</param>
+        /// <param name="dataToCopy">The collection of data items to copy.</param>
+        /// <param name="tranformToDataRow">A function that transforms each data item into an array of objects representing a row.</param>
+        /// <param name="logSql">Determines whether SQL statements should be logged. Defaults to <c>false</c>.</param>
+        /// <param name="logData">Determines whether data being copied should be logged. Defaults to <c>false</c>.</param>
+        /// <returns>
+        /// An <see cref="Either{Exception, long}"/> representing either an exception if the operation fails 
+        /// or the number of rows successfully copied.
+        /// </returns>
         public static async Task<Either<Exception,long>> CopyFromBinary<T>(
             this IDbConnection connection,
             string tableName,
             string columnToInsert,
             IEnumerable<T> dataToCopy,
-            Func<T, object[]> tranformToDataRow)
+            Func<T, object[]> tranformToDataRow,
+            bool logSql = false,
+            bool logData = false)
         {
             if (connection is not Npgsql.NpgsqlConnection npgsqlConnection)
                 throw new ArgumentException("Connection must be NpgsqlConnection");
             
             long returnValue = 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             try
             {
-                await npgsqlConnection.OpenAsync();
-                await using var writer = npgsqlConnection.BeginBinaryImport($"COPY {tableName} ({columnToInsert}) FROM STDIN (FORMAT BINARY)");
+                if(npgsqlConnection.State != ConnectionState.Open)
+                    await npgsqlConnection.OpenAsync();
+                var sql = $"COPY \"{tableName}\" ({columnToInsert}) FROM STDIN (FORMAT BINARY)";
+                if (logSql.Or(logData))
+                {
+                    Console.WriteLine($"Begin COPY FROM BINARY at {DateTime.Now.ToDateAndTimeFormat()}");
+                    if(logSql)
+                        Console.WriteLine($"{DateTime.Now.ToDateAndTimeFormat()} Executing : {sql}");
+                }
+                   
+                await using var writer = npgsqlConnection.BeginBinaryImport(sql);
                 foreach (var data in dataToCopy)
                 {
-                    writer.StartRow();
-                    writer.WriteRow(tranformToDataRow(data));
+                    object?[] row = tranformToDataRow(data);
+                    writer.WriteRow(row);
+                    if(logData)
+                        Console.WriteLine($"{DateTime.Now.ToDateAndTimeFormat()} Data : ({string.Join(",", row)})");
                 }
                 returnValue = (long)(await writer.CompleteAsync());
+                if (logSql.Or(logData))   
+                    Console.WriteLine($" COPY FROM BINARY COMPLETED at {DateTime.Now.ToDateAndTimeFormat()} Duration {(sw.ElapsedMilliseconds/1000).ToString("N2")} ");
+                
             }
             catch(Exception ex)
             {
+                if (logSql.Or(logData))
+                    Console.WriteLine($" COPY FROM BINARY FAILED at {DateTime.Now.ToDateAndTimeFormat()} Duration {(sw.ElapsedMilliseconds / 1000).ToString("N2")} \n Cause {ex.Message} ");
                 return ex;
             }
             finally
             {
+                sw.Stop();
                 if (connection.State != ConnectionState.Closed)
                     await npgsqlConnection.CloseAsync();
             }
             return returnValue;
         }
 
+        /// <summary>
+        /// Copies data from a CSV string into a database table asynchronously.
+        /// </summary>
+        /// <param name="connection">The database connection to use.</param>
+        /// <param name="tableName">The name of the table to insert data into.</param>
+        /// <param name="columnToInsert">The column in which the data will be inserted.</param>
+        /// <param name="csvData">The CSV-formatted string containing the data to be copied.</param>
+        /// <param name="logSql">Determines whether SQL statements should be logged. Defaults to <c>false</c>.</param>
+        /// <param name="logData">Determines whether data being copied should be logged. Defaults to <c>false</c>.</param>
+        /// <returns>
+        /// An <see cref="Either{Exception, long}"/> representing either an exception if the operation fails 
+        /// or the number of rows successfully copied.
+        /// </returns>
         public static async Task<Either<Exception, long>> CopyFromCSV(
             this IDbConnection connection,
             string tableName,
             string columnToInsert,
-            string csvData
+            string csvData,
+            bool logSql = false,
+            bool logData = false
             )
         {
             if (connection is not Npgsql.NpgsqlConnection npgsqlConnection)
                 throw new ArgumentException("Connection must be NpgsqlConnection");
             
             long returnValue = 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             try
             {
-                await npgsqlConnection.OpenAsync();
-                using var writer = npgsqlConnection.BeginTextImport($"COPY {tableName} ({columnToInsert}) FROM STDIN CSV");
+                if (npgsqlConnection.State != ConnectionState.Open)
+                    await npgsqlConnection.OpenAsync();
+                var sql = $"COPY \"{tableName}\" ({columnToInsert}) FROM STDIN CSV";
+                if (logSql.Or(logData))
+                {
+                    Console.WriteLine($"Begin COPY FROM CSV at {DateTime.Now.ToDateAndTimeFormat()}");
+                    if (logSql)
+                        Console.WriteLine($"{DateTime.Now.ToDateAndTimeFormat()} Executing : {sql}");
+                }
+                using var writer = npgsqlConnection.BeginTextImport(sql);
                 foreach (var line in csvData.Split('\n'))
                 {
+                    if (logData)
+                        Console.WriteLine($"{DateTime.Now.ToDateAndTimeFormat()} Data : ({line})");
                     await writer.WriteLineAsync(line);
                     returnValue++;
+                   
                 }
                 writer.Close();
+                if (logSql.Or(logData))
+                    Console.WriteLine($" COPY FROM CSV COMPLETED at {DateTime.Now.ToDateAndTimeFormat()} Duration {(sw.ElapsedMilliseconds / 1000).ToString("N2")} Seconds");
             }
             catch(Exception ex)
             {
+                if (logSql.Or(logData))
+                    Console.WriteLine($" COPY FROM CSV FAILED at {DateTime.Now.ToDateAndTimeFormat()} Duration {(sw.ElapsedMilliseconds / 1000).ToString("N2")} Seconds \n Cause {ex.Message}");
                 return ex;
             }
             finally
             {
-                if(connection.State != ConnectionState.Closed)
+                sw.Stop();
+                if (connection.State != ConnectionState.Closed)
                     await npgsqlConnection.CloseAsync();
             }
             return returnValue;
--- Src/RocketShop.Framework/Extension/ObjectExtension.cs
+++ Src/RocketShop.Framework/Extension/ObjectExtension.cs
@@ -56,6 +56,12 @@ namespace RocketShop.Framework.Extension
             return JsonSerializer.Serialize(obj).ToLower() == JsonSerializer.Serialize(comparer).ToLower();
         }
 
+        /// <summary>
+        /// Compares two objects at a binary level to determine if they are equal.
+        /// </summary>
+        /// <param name="obj">The first object to compare.</param>
+        /// <param name="comparer">The second object to compare.</param>
+        /// <returns><c>true</c> if the objects are binary equivalent; otherwise, <c>false</c>.</returns>
         public static bool BinaryEquals(this object? obj, object? comparer)
         {
             if (obj.IsNull() && comparer.IsNull())
@@ -66,11 +72,63 @@ namespace RocketShop.Framework.Extension
                 == Convert.ToBase64String(Encoding.UTF32.GetBytes(JsonSerializer.Serialize(comparer)));
         }
 
+        /// <summary>
+        /// Determines whether the specified object contains a property with the given name.
+        /// </summary>
+        /// <param name="obj">The object to check for the property.</param>
+        /// <param name="propertyName">The name of the property to look for.</param>
+        /// <returns><c>true</c> if the property exists; otherwise, <c>false</c>.</returns>
         public static bool HasProperty(this object? obj, string propertyName)
         {
             if (obj.IsNull())
                 return false;
             return obj!.GetType().GetProperty(propertyName) != null;
+        }
+
+        /// <summary>
+        /// Retrieves the property names of the given object.
+        /// </summary>
+        /// <param name="obj">The object whose property names are to be retrieved.</param>
+        /// <param name="includedDoubleqout">Determines whether property names should be enclosed in double quotes.</param>
+        /// <param name="skipColumns">An optional collection of property names to exclude from the result.</param>
+        /// <returns>An IEnumerable of property names as strings.</returns>
+        public static IEnumerable<string> GetPropertiesName(this object? obj,bool includedDoubleqout = false,IEnumerable<string>? skipColumns =null)
+        {
+            if (obj.IsNull())
+                return new List<string>();
+            var objType = obj!.GetType().GetProperties();
+            if(skipColumns.HasData())
+                objType = objType.Where(s =>!skipColumns!.Contains(s.Name)).ToArray();
+            if (includedDoubleqout)
+                return objType.Select(s => $"\"{s.Name}\"");
+            return objType.Select(s => s.Name);
+        }
+
+        /// <summary>
+        /// Merges a collection of strings into a single string, separated by the specified separator.
+        /// </summary>
+        /// <param name="strings">The collection of strings to merge.</param>
+        /// <param name="separator">The separator to use between strings. Defaults to ";".</param>
+        /// <returns>A single concatenated string with elements separated by the specified separator.</returns>
+        public static string Merged(this IEnumerable<string> strings, string separator =";") => string.Join(separator, strings);
+
+        /// <summary>
+        /// Converts an object's properties into an array of values.
+        /// </summary>
+        /// <param name="obj">The object to extract property values from.</param>
+        /// <param name="skipProperties">
+        /// A comma-separated string of property names to exclude from the result. Defaults to null.
+        /// </param>
+        /// <returns>An array of property values from the object.</returns>
+        public static object?[] ToObjectRows(this object? obj,string? skipProperties = null)
+        {
+            List<string> skipProp = new();
+            if (skipProperties.IsNotNull())
+                skipProp = skipProperties!.Split(',').ToList();
+            if (obj.IsNull())
+                return Array.Empty<object>();
+            var properties = obj!.GetType().GetProperties();
+            return skipProp.HasData() ? properties.Where(x=>!skipProp.Contains(x.Name)).Select(s => s.GetValue(obj)).ToArray() : properties.Select(s => s.GetValue(obj)).ToArray();
         }
     }
 }
